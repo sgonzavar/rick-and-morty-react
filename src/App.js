6085aaa3dbf1e99eebb8449f6caf0c72f8ee1cd2
Tiago <sgonzalezvargas.7cc@gmail.com>
2021-06-16 13:58:14 -0500

    Feat: init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+
+const App = () => {
+    return ( 
+        <Fragment> // fragment para encapsular todo el componente
+            <h1>Running</h1>
+        </Fragment>
+     );
+}
+ 
+export default App;
